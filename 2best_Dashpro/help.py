fe5d098d3cbd658514f946cdad0b354c45734484
Manupra1108 <103609505+Manupra1108@users.noreply.github.com>
2022-04-19 14:21:40 +0700

    Update help.py

--- 2best_Dashpro/help.py
+++ 2best_Dashpro/help.py
@@ -1,6 +1,7 @@
 def help(role):
     print("============ HELP ============")
-    if role == "Admin":
+    if role == "admin":
+        # sudah login sebagai admin
         print("register - untuk melakukan registrasi user baru")
         print("login - untuk melakukan login ke dalam sistem")
         print("tambah_game - untuk menambah game yang dijual pada toko")
@@ -11,7 +12,8 @@ def help(role):
         print("topup - untuk menambahkan saldo kepada user")
         print("save - untuk melakukan penyimpanan data ke dalam suatu file setelah melakukan perubahan")
         print("exit - untuk keluar dari aplikasi")
-    elif role == "User":
+    elif role == "user":
+        # sudah login sebagai user
         print("login - untuk melakukan login ke dalam sistem")
         print("list_game_toko - untuk melihat list game yang dijual pada toko")
         print("buy_game - Untuk membeli game")
@@ -21,23 +23,26 @@ def help(role):
         print("riwayat - untuk melihat riwayat pembelian game user")
         print("save - untuk melakukan penyimpanan data ke dalam suatu file setelah melakukan perubahan")
         print("exit - untuk keluar dari aplikasi")
+    else:
+        # Belum Login
+        print("login - untuk melakukan login ke dalam sistem")
     return
 
 
 # Fungsi :
-# 2_Register : Admin
-# 3_Login : User dan Admin
-# 4_tambah_game : Admin
+# 2_Register : Admin *
+# 3_Login : User dan Admin *
+# 4_tambah_game : Admin *
 # 5_ubah_game : Admin
-# 6_ubah_stok : Admin
-# 7_list_game_toko : User dan Admin
+# 6_ubah_stok : Admin *
+# 7_list_game_toko : User dan Admin **
 # 8_buy_game : User
-# 9_list_game : User
+# 9_list_game : User *
 # 10_search_my_game : User
-# 11_search_game_at_store : User dan Admin
-# 12_topup : Admin
-# 13_riwayat : User
-# 14_help
+# 11_search_game_at_store : User dan Admin **
+# 12_topup : Admin *
+# 13_riwayat : User *
+# 14_help *
 # 15_load
 # 16_save : User dan Admin
 # 17_exit : User dan Admin
