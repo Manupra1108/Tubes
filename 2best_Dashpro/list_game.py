73154f8d86dcc1a67f97ce2d29d49271dee9d100
Manupra1108 <103609505+Manupra1108@users.noreply.github.com>
2022-04-19 14:22:31 +0700

    Update list_game.py

--- 2best_Dashpro/list_game.py
+++ 2best_Dashpro/list_game.py
@@ -1,50 +1,44 @@
 from read_csv import read_csv
+from fungsi import baris,length,konso
 
-def baris_riwayat():
-    # Mencari jumlah baris pada data_riwayat_csv
-    baris = 0
-    for i in data_riwayat_csv:
-        baris +=1
-    return(baris)
-
-def array_game_id():
+def array_game_id(data1,id):
     # Membuat array berisikan kumpulan game_id yang dimiliki user 
     T_game_id = []
-    for i in range(1,baris_riwayat()):
-        for j in range(1):
-            T_game_id += [data_riwayat_csv[i][j]]
+    
+    for i in range(1,baris(data1)):
+        for j in range(3,4):
+            if id == data1[i][j]:
+                konso(T_game_id,data1[i][0])
+                #T_game_id += [data1[i][0]]
     return(T_game_id)
 
-def jumlah_game_id():
-    # Mencari jumlah game yang dimiliki user
-    count_game = 0
-    for arr in array_game_id():
-        count_game +=1
-    return(count_game)
-
-def sort_game_id():
+def sort_game_id(data1,id):
     # Melakukan sorting pada id_game
-    T_game_int = [0 for i in range(jumlah_game_id())]
+    len_game_id = length(array_game_id(data1,id))
+    game_int = ''
+    T_game_int = [0 for i in range(len_game_id)]
     j = 0
-    for i in range (jumlah_game_id()):
-        T_game_id = array_game_id()
-        arr = array_game_id()[i]
+    for i in range (len_game_id):
+        T_game_id = array_game_id(data1,id)
+        arr = array_game_id(data1,id)[i]
         terminated_id = False
         while not(terminated_id):
             if arr[j] == "0" or arr[j]=="G":
                 j +=1
             else:
-                T_game_int[i] = int(arr[j:])
+                while (j<=length(arr)-1):
+                    game_int += arr[j]
+                    j +=1
+                T_game_int[i] = int(game_int)
+                game_int = ''
                 j = 0
                 terminated_id = True
-    
     i = 0
     kondisi = False
     while not(kondisi):
         Pass = i
-        temp = 0
         imin = Pass
-        while(Pass<=jumlah_game_id()-1):
+        while(Pass<=len_game_id-1):
             if T_game_int[Pass] < T_game_int[imin]:
                 imin = Pass
                 Pass +=1
@@ -57,32 +51,33 @@ def sort_game_id():
         T_game_int[imin] = temp_int
         T_game_id[imin] = temp_id
         i +=1
-        if i == jumlah_game_id():
+        if i == len_game_id:
             kondisi = True
     return(T_game_id)
 
-def baris_game():
-    # Mencari jumlah baris pada data_game_csv
-    baris = 0
-    for i in data_game_csv:
-        baris += 1
-    return(baris)
 
-def list_game():
-    if baris_riwayat() == 1:
+def list_game(data1,data2,id):
+    len_game_id = length(array_game_id(data1,id))
+    if len_game_id == 0:
         print("Maaf, kamu belum memiliki game. Ketik perintah beli_game untuk beli")
     else:
         count = 0
-        for i in range(baris_game()):
-            if jumlah_game_id() == count:
-                break
-            for j in range(1):
-                if data_game_csv[i][j] == sort_game_id()[count]:
-                    print(data_game_csv[i][0], '|' , data_game_csv[i][1], '|',data_game_csv[i][2], '|' , data_game_csv[i][3], '|', data_game_csv[i][4])
-                    count +=1
+        i = 0
+        j = 0
+        while (count<len_game_id):
+            if data2[i][j] == sort_game_id(data1,id)[count]:
+                print(data2[i][0], '|' , data2[i][1], '|',data2[i][2], '|' , data2[i][3], '|', data2[i][4])
+                count +=1
+                i = 0
+            else:
+                i +=1
     return
-                
-        
-data_game_csv = read_csv('game.csv')
-data_riwayat_csv = read_csv('riwayat.csv')
-list_game()
+
+
+if __name__ == '__main__': 
+    id = input()
+    data_riwayat_csv = read_csv('riwayat.csv')
+    data_game_csv = read_csv('game.csv')
+    print(array_game_id(data_riwayat_csv,id))
+    print(sort_game_id(data_riwayat_csv,id))
+    list_game(data_riwayat_csv,data_game_csv,id)
