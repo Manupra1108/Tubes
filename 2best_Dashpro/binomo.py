bd77a56f54c1bb284ba9b36b1d023d01acf6bd82
Manupra1108 <103609505+Manupra1108@users.noreply.github.com>
2022-04-21 12:34:38 +0700

    Update binomo.py

--- 2best_Dashpro/binomo.py
+++ 2best_Dashpro/binomo.py
@@ -1,41 +1,39 @@
-import os
-from fungsi import baris, length
-from read_csv import read_csv
-from login import login
-from register import register
-from tambah_game import tambah_game
-from f06 import ubah_stok
-from list_game_toko import list_game_toko
-from list_game import list_game
+from fungsi import length
+from F02 import register
+from F03 import login
+from F04 import tambah_game
+from F05 import ubah_game
+from F06 import ubah_stok
+from F07 import list_game_toko
+from F08 import buy_game
+from F09 import list_game
+from F10 import search_my_game
 from F11 import search_game_at_store
-from f12 import topup
-from riwayat import riwayat
-from help import help
-from load import load
+from F12 import topup
+from F13 import riwayat
+from F14 import help
+from F15 import load
+from F16 import save
+from F17 import exit
 
-folder_path = str(os.getcwd())
+#folder_path = str(os.getcwd())
 cek_load = False
-if load(folder_path) != []:
+if load('semua') != []:
     cek_load = True
 
 if cek_load:
     # Load file sukses
     print("Loading...")
+    data_game_csv = load('game.csv')
+    data_kepemilikan_csv = load('kepemilikan.csv')
+    data_riwayat_csv = load('riwayat.csv')
+    data_user_csv = load('user.csv')
     print("Selamat datang di antarmuka “Binomo!”")
-    data_game_csv = load(folder_path)[0]
-    data_kepemilikan_csv = load(folder_path)[1]
-    data_riwayat_csv = load(folder_path)[2]
-    data_user_csv = load(folder_path)[3]
-
-#data_game_csv = read_csv('game.csv')
-#data_user_csv = read_csv("user.csv")
-#data_riwayat_csv = read_csv('riwayat.csv')
-#data_kepemilikan_csv = read_csv('kepemilikan.csv')
-
-    print("Data_game =", data_game_csv)
-    print("Data_user =",data_user_csv)
-    print("Data_riwayat = " ,data_riwayat_csv)
-    print("Data_kepemilikan = ",data_kepemilikan_csv)
+    print()
+    #print("Data_game =", data_game_csv)
+    #print("Data_user =",data_user_csv)
+    #print("Data_riwayat = " ,data_riwayat_csv)
+    #print("Data_kepemilikan = ",data_kepemilikan_csv)
 
     cek_login = False
     while cek_login == False:
@@ -55,7 +53,7 @@ if cek_load:
             print()
 
     # Get Role
-    for i in range(baris(data_user_csv)):
+    for i in range(length(data_user_csv)):
         for j in range(1):
             if id_user == data_user_csv[i][j]:
                 role = data_user_csv[i][4]
@@ -67,6 +65,8 @@ if cek_load:
             perintah = input(">>> ")
             if perintah == 'register':
                 register(data_user_csv)
+            elif perintah == 'ubah_game':
+                ubah_game(data_game_csv)
             elif perintah == 'tambah_game':
                 tambah_game(data_game_csv)
             elif perintah == 'ubah_stok':
@@ -79,23 +79,36 @@ if cek_load:
                 topup(data_user_csv)
             elif perintah == 'help':
                 help(role)
+            elif perintah == "save":
+                save(data_user_csv, data_game_csv, data_kepemilikan_csv, data_riwayat_csv)
+            elif perintah == 'exit':
+                exit(data_user_csv, data_game_csv, data_kepemilikan_csv, data_riwayat_csv)
             elif perintah == 'hanya_user':
                 print("Maaf, anda harus menjadi user untuk melakukan hal tersebut.")
             else:
                 print("Perintah tidak valid")
+
         elif role == 'user':
             print()
             perintah = input(">>> ")
             if perintah == 'list_game_toko':
                 list_game_toko(data_game_csv)
             elif perintah == 'list_game':
-                list_game(data_riwayat_csv,data_game_csv,id_user)
+                list_game(data_kepemilikan_csv,data_game_csv,id_user)
+            elif perintah == 'buy_game':
+                buy_game(data_riwayat_csv,data_game_csv,data_kepemilikan_csv,data_user_csv,id_user)
+            elif perintah == 'search_my_game':
+                search_my_game(data_kepemilikan_csv,data_game_csv,id_user)
             elif perintah == 'search_game_at_store':
                 search_game_at_store(data_game_csv)
             elif perintah == 'riwayat':
                 riwayat(data_riwayat_csv,id_user)
             elif perintah == 'help':
                 help(role)
+            elif perintah == "save":
+                save(data_user_csv, data_game_csv, data_kepemilikan_csv, data_riwayat_csv)
+            elif perintah == 'exit':
+                exit(data_user_csv, data_game_csv, data_kepemilikan_csv, data_riwayat_csv)
             elif perintah == 'hanya_admin':
                 print("Maaf, anda tidak memiliki izin untuk menjalankan perintah berikut. Mintalah ke administrator untuk melakukan hal tersebut.")
             else:
